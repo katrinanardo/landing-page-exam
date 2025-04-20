22ec8da6d1fdc30245da69923486a142230c364b
katrinanardo <99706445+katrinanardo@users.noreply.github.com>
2025-04-20 20:36:36 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,8 +14,10 @@ const Header = () => {
       <img src={Logo} alt='' />
     </Link>
       <div className='flex items-center gap-6'>
-      <Link className='hover:text-violet-900 transition' to=''>Link</Link>
-      <Link className='hover:text-violet-900 transition' to=''>Log In</Link>
+      <Link className='hover:text-violet-900 transition' to=''>Home</Link>
+      <Link className='hover:text-violet-900 transition' to=''>About Us</Link>
+      <Link className='hover:text-violet-900 transition' to=''>Listings</Link>
+      <Link className='hover:text-violet-900 transition' to=''>Contact</Link>
         <Link className='bg-orange-600 hover:bg-orange-800 text-white md:px-4 md:py-3 px-2 py-2 md:text-[16px] text-[12px] rounded-tr-[20px] transition'to=''>Work with us</Link>
 
       </div>
